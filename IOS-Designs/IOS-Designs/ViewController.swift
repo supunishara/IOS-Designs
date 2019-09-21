be712fd4849db0595ed78c3943af21a6bf3d6875
Supun Ishara <supun.ishara@i4tradies.com>
2019-09-21 23:18:43 +0530

    ïnitial Xcode Project

---
+++ IOS-Designs/IOS-Designs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS-Designs
+//
+//  Created by Supun Ishara  on 9/21/19.
+//  Copyright © 2019 vimukthi visvanath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
